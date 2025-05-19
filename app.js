acaf2ed3e0d5396fe500a5fea884bddae1c086b3
Maheswari8074 <maheswarimudadla756@gmail.com>
2025-05-19 12:38:09 +0530

    initialn1

---
+++ app.js
@@ -0,0 +1,11 @@
+let userScore=0;
+let comScore=0;
+
+const choices =document.querySelectorAll(".choice");
+
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+           console.log("choice was clicked");
+           
+    });
+});
